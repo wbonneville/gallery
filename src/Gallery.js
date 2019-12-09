1af971013b8f9eb55790c2094f5aeb743bc0674f
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-09 11:47:28 -0500

    add gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,20 +3,25 @@ import styled from "styled-components";
 import { data } from "./data";
 
 const Styles = styled.div`
-  display: flex;
+  flex-direction: row;
+  margin-left: 3%;
+  margin-right: 3%;
 `;
 
-const Img = styled.img`
-  width: 20%;
+const Div = styled.div`
+  margin-top: 12px;
+  & img {
+    width: 100%;
+  }
 `;
 
 const Gallery = () => {
   return (
-    <Styles>
+    <Styles className="row">
       {data.map(item => (
-        <div key={item.sys.id}>
-          <Img alt="fall" src={item.fields.image.file.url} />
-        </div>
+        <Div className="col-xs-3">
+          <img key={item.sys.id} alt="fall" src={item.fields.image.file.url} />
+        </Div>
       ))}
     </Styles>
   );
--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import image1 from "./images/fall1.jpg";
+import image2 from "./images/spring1.jpg";
 
 export const data = [
   {
@@ -22,27 +23,115 @@ export const data = [
         }
       }
     }
-  }
-  // {
-  //   sys: {
-  //     id: "1"
-  //   },
-  //   fields: {
-  //     name: "a beautiful photo",
-  //     type: "vertical",
-  //     animal: false,
-  //     person: false,
-  //     spring: false,
-  //     summer: false,
-  //     fall: false,
-  //     winter: true,
-  //     description: "This is a beautiful photo that I took.",
+  },
+  {
+    sys: {
+      id: "2"
+    },
+    fields: {
+      name: "a beautiful photo",
+      type: "vertical",
+      animal: false,
+      person: false,
+      spring: false,
+      summer: false,
+      fall: false,
+      winter: true,
+      description: "This is a beautiful photo that I took.",
 
-  //     image: {
-  //       file: {
-  //         url: image1
-  //       }
-  //     }
-  //   }
-  // }
+      image: {
+        file: {
+          url: image2
+        }
+      }
+    }
+  },
+  {
+    sys: {
+      id: "3"
+    },
+    fields: {
+      name: "a beautiful photo",
+      type: "vertical",
+      animal: false,
+      person: false,
+      spring: false,
+      summer: false,
+      fall: false,
+      winter: true,
+      description: "This is a beautiful photo that I took.",
+
+      image: {
+        file: {
+          url: image2
+        }
+      }
+    }
+  },
+  {
+    sys: {
+      id: "4"
+    },
+    fields: {
+      name: "a beautiful photo",
+      type: "vertical",
+      animal: false,
+      person: false,
+      spring: false,
+      summer: false,
+      fall: false,
+      winter: true,
+      description: "This is a beautiful photo that I took.",
+
+      image: {
+        file: {
+          url: image2
+        }
+      }
+    }
+  },
+  {
+    sys: {
+      id: "5"
+    },
+    fields: {
+      name: "a beautiful photo",
+      type: "vertical",
+      animal: false,
+      person: false,
+      spring: false,
+      summer: false,
+      fall: false,
+      winter: true,
+      description: "This is a beautiful photo that I took.",
+
+      image: {
+        file: {
+          url: image2
+        }
+      }
+    }
+  },
+  {
+    sys: {
+      id: "6"
+    },
+    fields: {
+      name: "a beautiful photo",
+      type: "vertical",
+      animal: false,
+      person: false,
+      spring: false,
+      summer: false,
+      fall: false,
+      winter: true,
+      description: "This is a beautiful photo that I took.",
+
+      image: {
+        file: {
+          url: image2
+        }
+      }
+    }
+  }
 ];
