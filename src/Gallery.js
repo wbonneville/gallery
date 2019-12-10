cca0463eb8890650d98b0534891b29662b16e5fc
Wesley Bonneville <westazbonneville@gmail.com>
2019-12-10 14:37:25 -0500

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -21,6 +21,36 @@ const Styles = styled.div`
 
 const Div = styled.div``;
 
+const useHover = () => {
+  const ref = useRef();
+  const [hovered, setHovered] = useState(false);
+
+  const enter = () => setHovered(true);
+  const leave = () => setHovered(false);
+
+  useEffect(() => {
+    ref.current.addEventListener("mouseenter", enter);
+    ref.current.addEventListener("mouseleave", leave);
+
+    return () => {
+      ref.current.removeEventListener("mouseenter", enter);
+      ref.current.removeEventListener("mouseleave", leave);
+    };
+  }, [ref]);
+
+  return [ref, hovered];
+};
+
+const MyImage = ({ src, header }) => {
+  const [ref, hovered] = useHover();
+  return (
+    <div className="row imageSpace">
+      {hovered && <h1>{header}</h1>}
+      <img ref={ref} className="image" alt="fall" src={src} />
+    </div>
+  );
+};
+
 const Gallery = ({ initialData, initialDataSetTwo, initialDataSetThree }) => {
   const [data, setData] = useState(initialData);
   const [dataTwo, setDataTwo] = useState(initialDataSetTwo);
@@ -31,14 +61,11 @@ const Gallery = ({ initialData, initialDataSetTwo, initialDataSetThree }) => {
       <Styles className="row">
         <Div className="col-xs-4">
           {data.map(item => (
-            <div className="row imageSpace">
-              <img
-                className="image"
-                key={item.sys.id}
-                alt="fall"
-                src={item.fields.image.file.url}
-              />
-            </div>
+            <MyImage
+              key={item.sys.id}
+              src={item.fields.image.file.url}
+              header={item.fields.name}
+            />
           ))}
         </Div>
         <Div className="col-xs-4">
@@ -49,6 +76,7 @@ const Gallery = ({ initialData, initialDataSetTwo, initialDataSetThree }) => {
                 key={item.sys.id}
                 alt="fall"
                 src={item.fields.image.file.url}
+                header={item.fields.name}
               />
             </div>
           ))}
